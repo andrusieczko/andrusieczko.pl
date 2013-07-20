6d77162415e468bf5afea27244e6f141a87726f7
andrusieczko <karol.andrusieczko@gmail.com>
2013-07-20 16:52:16 +0200

    first version of design + btn-collapse idea

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+var containsClass = function($el, clazz) {
+  return $el.attr('class').indexOf(clazz) >= 0;
+};
+
 $(function() {
 	prettyPrint();
+
+  var $btnCollapse = $('.btn-collapse');
+  $btnCollapse.find('.btn').live('click', function(ev) {
+    var $btn = $(ev.target);
+    var $theBtnCollapse = $btn.parent('.btn-collapse');
+    var $box = $theBtnCollapse.find('.box');
+
+    if (!containsClass($box, 'in') && !containsClass($box, 'out')) {
+      $box.addClass('in');
+    } else if (containsClass($box, 'in')) {
+      $box.removeClass('in');
+      $box.addClass('out');
+    } else {
+      $box.removeClass('out');
+      $box.addClass('in');
+    }
+  });
+
 });
+
+function Ctrl($scope) {
+  $scope.tpl = function(template) {
+    return template + ".html";
+  };
+}
